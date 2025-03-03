aa29736db44c4b7817f87dbdec9e0f88475b500d
rkrzy <81816761+rkrzy@users.noreply.github.com>
2025-03-03 14:14:46 +0900

    init setting

---
+++ src/main/java/com/example/modern_api/ModernApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.modern_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModernApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/modern_api/ModernApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.modern_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
